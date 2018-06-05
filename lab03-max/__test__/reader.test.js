a32bbd9635ac7d670c9ba3218a7c35e9eff362e4
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-06-05 10:33:48 -0700

    resubmitting lab 03

--- lab03-max/__test__/reader.test.js
+++ lab03-max/__test__/reader.test.js
@@ -1,54 +1,31 @@
 'use strict';
 
-let read = require('../lib/reader.js');
+let reader = require('../lib/reader.js');
 
 describe('Reader Module', () => {
 
 
-  it('should callback with error for a non-existent file', (done) => {
-    read(['missing.txt'], (err) => {
+  it('should callback with error for a non-existent file', function(done) {
+    reader(['missing.txt'], (err) => {
+      // expect(err).toMatch(/ENOENT/);
       expect(err).not.toBeUndefined();
       done();
     });
   });
 
-  // it('should callback with file contents of one file', (done) => {
+  it('should callback with file contents of multiple files in order', function(done) {
   
-  //   const expected = 'words about apples'; 
+    const expected1 = 'words about apples'; 
+    const expected2 = 'words about bananas';
+    const expected3 = /cucumbers/;
 
-  //   read([`${__dirname}/../data/apples.txt`], (err, data) => {
+    reader([`${__dirname}/../data/apples.txt`,`${__dirname}/../data/bananas.txt`,`${__dirname}/../data/cucumbers.txt`], (err, data) => {
+      expect(err).toBeUndefined();
+      expect(data[0]).toBe(expected1);
+      expect(data[1]).toBe(expected2);
+      expect(data[2]).toMatch(expected3);
 
-  //     expect(err).toBeUndefined();
-  //     let actual = data.toString();
-  //     expect(actual).toBe(expected);
-
-  //     done();
-      
-  //   });
-  // });
-
-  it('should callback with file contents of multiple files in order', (done) => {
-  
-    let paths = [];
-    for (let item of ['cucumbers','apples','bananas']) {
-      paths.push(__dirname + '/../data/' + item + '.txt');
-    }
-    let expected, actual;
-    read(paths, (err, contents) => {
-      expect(err).not.toBeUndefined();
-      expected = true;
-      actual = contents[0].toString().startsWith('cucumber');
-      expect(actual).toBe(expected);
-
-      expected = 'words about apples'; 
-      actual = contents[1].toString();
-      expect(actual).toBe(expected);
-
-      expected = 'words about bananas'; 
-      actual = contents[2].toString();
-      expect(actual).toBe(expected);
-
-      done();      
+      done();
     });
   });
 
--- lab03-max/lib/reader.js
+++ lab03-max/lib/reader.js
@@ -1,19 +1,23 @@
 'use strict';
 
 const fs = require('fs');
+let newArr = [];
 
-module.exports = exports = (arr,callback) => {
-  let newArr = [];
-  for(let i = 0; i < arr.length; i++) {
-    fs.readFile(arr[i], (err,data) => {
-      if (err) {
-        return callback(err);
-      } else {
-        newArr[i] = data.toString().trim();
-        if (newArr.length === arr.length) {
-          callback(undefined, newArr);
-        }
-      }
+module.exports = exports = function(arr,callback) {
+
+  fs.readFile(arr[0], (err, data) => {
+    if(err) return callback(err); 
+    newArr[0] = data.toString().trim();
+
+    fs.readFile(arr[1], (err, data) => {
+      if(err) return callback(err); 
+      newArr[1] = data.toString().trim();
+
+      fs.readFile(arr[2], (err, data) => {
+        if(err) return callback(err); 
+        newArr[2] = data.toString().trim();
+        return callback(undefined, newArr);
+      });
     });
-  }
+  });
 };
